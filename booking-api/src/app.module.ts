ec2ba4c96197558b0b5673e0a865245b0786d039
khaledosman <khaled.osmaan@gmail.com>
2022-01-27 15:46:32 +0100

    fix: enable playground in production

--- booking-api/src/app.module.ts
+++ booking-api/src/app.module.ts
@@ -14,6 +14,7 @@ import { join } from 'path'
     ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       // autoSchemaFile: true,
+      playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true
     }),
